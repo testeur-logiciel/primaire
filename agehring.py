add542cde089a10d0f086df89e8dc816ee3f6a86
testeur-logiciel <andrewgehring@Andrews-Mac-mini.local>
2023-09-26 09:46:42 -0600

    Update besoke file

--- agehring.py
+++ agehring.py
@@ -1,3 +1,21 @@
 #This file is only in agehring/primaire feature/SecurityIssue1
 
-print('Hello, Andrew!')
+def _SendTemplateResponseAMG(self, filename, specials, params,
+                            new_cookie_text=None):
+    """Sends a response using a gtl template.
+
+    Args:
+      filename: The template file.
+      specials: Other special values for this request.
+      params: Cgi parameters.
+      new_cookie_text: New cookie to set.
+    """
+    f = None
+    try:
+      f = _Open(RESOURCE_PATH, filename)
+      template = f.read()
+    finally:
+      if f: f.close()
+    self._SendHtmlResponse(
+        gtl.ExpandTemplate(template, specials, params),
+        new_cookie_text)
